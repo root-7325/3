fff3d88045d6c3adfeee88d50d86bb14ec636b0e
root <69764760+root-7325@users.noreply.github.com>
2022-07-06 19:49:14 +0300

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,40 @@
+using System;
+class MultiplayerHouse
+{
+    static void Main()
+    {
+        string[] familiya = new string[10];
+        int kvartira = 0;
+        int forexception = 0;
+        Console.WriteLine("Input 10 familiy");
+        for (int i = 0; i < 10; i++)
+        {
+            familiya[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Input 3 kvartir");
+        for (int i = 0; i < 3; i++)
+        {
+            try
+            {
+                kvartira = Convert.ToInt32(Console.ReadLine()) - 1;
+            }
+            catch (System.FormatException)
+            {
+                forexception++;
+            }
+            if (kvartira > 9 || forexception == 1)
+            {
+                Console.WriteLine("Wrong number!");
+                forexception--;
+                if (i != 0)
+                {
+                    i--;
+                }
+            }
+            else
+            {
+                Console.WriteLine(familiya[kvartira]);
+            }
+        }
+    }
+}
