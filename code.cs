9f65a50a4ac3b70ca9e28b51640d6b61746fb883
root <69764760+root-7325@users.noreply.github.com>
2022-07-06 20:07:11 +0300

    Update code.cs

--- code.cs
+++ code.cs
@@ -11,17 +11,22 @@ class MultiplayerHouse
         {
             familiya[i] = Console.ReadLine();
         }
+
         Console.WriteLine("Input 3 kvartir");
+
         for (int i = 0; i < 3; i++)
         {
+
             try
             {
                 kvartira = Convert.ToInt32(Console.ReadLine()) - 1;
             }
+
             catch (System.FormatException)
             {
                 forexception++;
             }
+
             if (kvartira > 9 || forexception == 1)
             {
                 Console.WriteLine("Wrong number!");
@@ -29,15 +34,19 @@ class MultiplayerHouse
                 {
                     forexception--;
                 }
+
                 if (i != 0)
                 {
                     i--;
                 }
+
             }
+
             else
             {
                 Console.WriteLine(familiya[kvartira]);
             }
+
         }
         Console.ReadKey(true);
     }
